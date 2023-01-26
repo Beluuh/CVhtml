c813ffc5cea26683dd6379735375f7435f7401c0
belythom@gmail.com <belythom@gmail.com>
2023-01-26 19:55:22 -0300

    2docommit26ene

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var jcontent={
   "ocupacion": "Ocupación: Desarrollo Web",
+  "nacionalidad":"Nacionalidad:Argentina",
 }
 var output = document.getElementById('output');
 output.innerHTML = jcontent.ocupacion;
+
+var output = document.getElementById('output2');
+output.innerHTML = jcontent.nacionalidad;

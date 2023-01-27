75cc7a21077db68502d605f50c4317449cdb3335
belythom@gmail.com <belythom@gmail.com>
2023-01-26 23:11:55 -0300

    3ercommit26ene

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 var jcontent={
   "ocupacion": "Ocupación: Desarrollo Web",
   "nacionalidad":"Nacionalidad:Argentina",
+  "dni":"DNI: 35.677.889",
+  "nacimiento":"Fecha de Nacimiento 28/10/1990",
+   "edad":"Edad: 32",
+   "genero":"Género: Femenino",
+   "direccion":"Dirección: Lavalle 2212",
+  "localidad":"Localidad: Abasto",
+  "codigo":"Código postal1234",
 }
 var output = document.getElementById('output');
 output.innerHTML = jcontent.ocupacion;
 
 var output = document.getElementById('output2');
 output.innerHTML = jcontent.nacionalidad;
+
+var output = document.getElementById('output3');
+output.innerHTML = jcontent.dni;
+
+var output = document.getElementById('output4');
+output.innerHTML = jcontent.nacimiento;
+
+var output = document.getElementById('output5');
+output.innerHTML = jcontent.edad;
+
+var output = document.getElementById('output6');
+output.innerHTML = jcontent.genero;
+
+var output = document.getElementById('output7');
+output.innerHTML = jcontent.direccion;
+
+var output = document.getElementById('output8');
+output.innerHTML = jcontent.localidad;;
+
+var output = document.getElementById('output9');
+output.innerHTML = jcontent.codigo;
